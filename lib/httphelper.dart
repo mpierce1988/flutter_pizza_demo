bc37e7d1304f91cfe3649a183df1b3164a46a533
mpierce1988 <mpierce1988@gmail.com>
2022-05-24 19:44:25 -0300

    Completed httphelper.dart

---
+++ lib/httphelper.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'pizza.dart';
+
+class HttpHelper {
+  final String authority = 'wj08k.mocklab.io';
+  final String path = 'pizzalist';
+
+  Future<List<Pizza>?> getPizzaList() async {
+    // create url to online json, using the correct scheme/format
+    Uri url = Uri.https(authority, path);
+
+    // sent HTTP GET request, wait for results, store in variable 'result'
+    http.Response result = await http.get(url);
+
+    // if result status is ok, decode the body of the result into a list of
+    // pizzas
+    if (result.statusCode == HttpStatus.ok) {
+      // decode body of result using json convert function from dart:convert
+      final jsonResponse = json.decode(result.body);
+      // provide a type argument to the map method to avoid a type error
+      List<Pizza> pizzas =
+          jsonResponse.map<Pizza>((i) => Pizza.fromJsonOrNull(i)).toList();
+
+      return pizzas;
+    }
+    return null;
+  }
+}
